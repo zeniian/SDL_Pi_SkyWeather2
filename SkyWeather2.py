6f2b1a0b536bdfc0c742a003ea17b099736dd955
Zach Marano <zmarano@gmail.com>
2021-06-27 22:53:13 -0700

    Allow pigpiod to be started if we are root.

--- SkyWeather2.py
+++ SkyWeather2.py
@@ -90,6 +90,25 @@ if (config.enable_MySQL_Logging):
         sys.exit("SkyWeather2 Requirements Error Exit")
 
 
+def startPigpiod():
+    if (config.SWDEBUG):
+        print("Starting pigpio daemon")
+
+    # kill all pigpio instances
+    try:
+        cmd = [ 'killall', 'pigpiod' ]
+        output = subprocess.check_output(cmd, stderr=subprocess.STDOUT)
+        print(output)
+        time.sleep(5)
+    except:
+        #print(traceback.format_exc())
+        pass
+
+    cmd = [ '/usr/bin/pigpiod' ]
+    output = subprocess.check_output(cmd, stderr=subprocess.STDOUT)
+    print(output)
+
+
 # main program
 print ("")
 
@@ -100,23 +119,11 @@ print ("Program Started at:"+ time.strftime("%Y-%m-%d %H:%M:%S"))
 print ("##########################################################")
 print ("")
 
-#
-if (config.SWDEBUG):
-    print("Starting pigpio daemon")
+if os.geteuid() != 0:
+    print("Make sure pigpiod is started.")
+else:
+    startPigpiod()
 
-# kill all pigpio instances
-try:
-    cmd = [ 'killall', 'pigpiod' ]
-    output = subprocess.check_output(cmd, stderr=subprocess.STDOUT)
-    print(output)
-    time.sleep(5)
-except:
-    #print(traceback.format_exc())
-    pass
-
-cmd = [ '/usr/bin/pigpiod' ]
-output = subprocess.check_output(cmd, stderr=subprocess.STDOUT)
-print(output)
 import DustSensor
 
 # detect devices
